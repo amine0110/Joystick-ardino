7d6ba56c1cc3f153547bdd9984262953905dd99a
amine0110 <37108394+amine0110@users.noreply.github.com>
2018-03-06 15:24:29 +0100

    Add files via upload

---
+++ sketch_feb09a.ino
@@ -0,0 +1,40 @@
+#include <Servo.h>
+Servo amine;
+
+byte led;
+
+int joyx= 0;
+int joyy= 1;
+
+int joyval; 
+
+
+
+
+void setup() {
+  amine.attach(3);
+  
+  for(byte n=4; n<13;n++){
+     pinMode(n , OUTPUT);
+  
+  }
+  
+
+}
+
+void loop() {
+  joyval = analogRead(joyx);
+  joyval = map(joyval, 0, 1023, 0, 180);
+  amine.write(joyval);
+
+  joyval = analogRead(joyy);
+  led = map(joyval, 0, 1023, 4, 12);
+
+  digitalWrite(led, 1);
+  delay(10);
+  digitalWrite(led, 0);
+
+  
+  delay(15);
+
+}
